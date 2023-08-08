b9623c83ad4abc6b9f8bebd5e83edbf4c92de3f4
Daniil Robnikov <daniilrobnikov@gmail.com>
2023-08-08 11:48:34 +0300

    Update monotonic align to cuda version

--- monotonic_align.py
+++ monotonic_align.py
@@ -1,6 +1,68 @@
 import torch
 import numba
+from numba import cuda
 import numpy as np
+
+
+# ! ----------------------------- monotonic_align.py -----------------------------
+
+
+@cuda.jit("void(int32[:,:,:], float32[:,:,:], int32[:], int32[:])")
+def maximum_path_cuda_jit(paths, values, t_ys, t_xs):
+    max_neg_val = -1e9
+    i = cuda.grid(1)
+    if i >= paths.shape[0]:  # exit if the thread is out of the index range
+        return
+
+    path = paths[i]
+    value = values[i]
+    t_y = t_ys[i]
+    t_x = t_xs[i]
+
+    v_prev = v_cur = 0.0
+    index = t_x - 1
+
+    for y in range(t_y):
+        for x in range(max(0, t_x + y - t_y), min(t_x, y + 1)):
+            v_cur = value[y - 1, x] if x != y else max_neg_val
+            v_prev = value[y - 1, x - 1] if x != 0 else (0.0 if y == 0 else max_neg_val)
+            value[y, x] += max(v_prev, v_cur)
+
+    for y in range(t_y - 1, -1, -1):
+        path[y, index] = 1
+        if index != 0 and (index == y or value[y - 1, index] < value[y - 1, index - 1]):
+            index = index - 1
+
+    # Wait for other threads in this block
+    cuda.syncthreads()
+
+
+def maximum_path_cuda(neg_cent: torch.Tensor, mask: torch.Tensor):
+    """
+    Monotonic alignment search algorithm
+    value: [b, t_x, t_y]
+    mask: [b, t_x, t_y]
+    """
+    device = neg_cent.device
+    dtype = neg_cent.dtype
+
+    neg_cent_device = cuda.as_cuda_array(neg_cent)
+    path_device = cuda.device_array(neg_cent.shape, dtype=np.int32)
+    t_t_max_device = cuda.as_cuda_array(mask.sum(1, dtype=torch.int32)[:, 0])
+    t_s_max_device = cuda.as_cuda_array(mask.sum(2, dtype=torch.int32)[:, 0])
+
+    threadsperblock = 32
+    blockspergrid = (path_device.shape[0] + (threadsperblock - 1)) // threadsperblock
+
+    maximum_path_cuda_jit[blockspergrid, threadsperblock](path_device, neg_cent_device, t_t_max_device, t_s_max_device)
+
+    # Convert device array back to tensor
+    path = torch.as_tensor(path_device.copy_to_host(), device=device, dtype=dtype)
+
+    return path
+
+
+# ! ----------------------------- monotonic_align.py -----------------------------
 
 
 def maximum_path(neg_cent, mask):
